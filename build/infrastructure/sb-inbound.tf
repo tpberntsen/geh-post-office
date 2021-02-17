41ae59907ab42d743caf1b98203b13a7d2e59297
René Nielsen <renetdev@gmail.com>
2021-02-17 23:29:50 +0100

    fix bug in tf module dependencies

--- build/infrastructure/sb-inbound.tf
+++ build/infrastructure/sb-inbound.tf
@@ -11,18 +11,16 @@ module "sbnar_inbound_listener" {
   source                    = "git::https://github.com/Energinet-DataHub/green-energy-hub-core.git//terraform/modules/service-bus-topic-auth-rule?ref=1.2.0"
   name                      = "sbnar-inbound-listener"
   namespace_name            = module.sbn_inbound.name
-  topic_name                = module.sbt_inbound.name
   resource_group_name       = data.azurerm_resource_group.postoffice.name
   listen                    = true
-  dependencies              = [module.sbt_inbound]
+  dependencies              = [module.sbn_inbound]
 }
 
 module "sbnar_inbound_sender" {
   source                    = "git::https://github.com/Energinet-DataHub/green-energy-hub-core.git//terraform/modules/service-bus-topic-auth-rule?ref=1.2.0"
   name                      = "sbnar-inbound-sender"
   namespace_name            = module.sbn_inbound.name
-  topic_name                = module.sbt_inbound.name
   resource_group_name       = data.azurerm_resource_group.postoffice.name
   send                      = true
-  dependencies              = [module.sbt_inbound]
+  dependencies              = [module.sbn_inbound]
 }
