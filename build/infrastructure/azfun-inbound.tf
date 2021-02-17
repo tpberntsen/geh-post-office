a6b122e31f84c0f04585b03816b9378033928931
René Nielsen <renetdev@gmail.com>
2021-02-17 13:34:43 +0100

    impl

--- build/infrastructure/azfun-inbound.tf
+++ build/infrastructure/azfun-inbound.tf
@@ -9,9 +9,10 @@ module "azfun_inbound" {
   application_insights_instrumentation_key  = module.appi_postoffice.instrumentation_key
   tags                                      = data.azurerm_resource_group.postoffice.tags
   app_settings                              = {
-    POSTOFFICE_DB_CONNECTION_STRING     = azurerm_cosmosdb_account.postoffice.endpoint,
-    POSTOFFICE_DB_KEY                   = azurerm_cosmosdb_account.postoffice.primary_key
-    INBOUND_QUEUE_MARKETDATA_TOPIC_NAME = module.sbt_marketdata.name
+    POSTOFFICE_DB_CONNECTION_STRING             = azurerm_cosmosdb_account.postoffice.endpoint,
+    POSTOFFICE_DB_KEY                           = azurerm_cosmosdb_account.postoffice.primary_key
+    INBOUND_QUEUE_MARKETDATA_TOPIC_NAME         = module.sbt_marketdata.name
+    INBOUND_QUEUE_MARKETDATA_SUBSCRIPTION_NAME  = module.sbt_marketdata.name
   }
   dependencies                              = [
     module.azfun_inbound_plan.dependent_on,
--- build/infrastructure/sb-inbound.tf
+++ build/infrastructure/sb-inbound.tf
@@ -6,31 +6,3 @@ module "sbn_inbound" {
   sku                 = "basic"
   tags                = data.azurerm_resource_group.postoffice.tags
 }
-
-module "sbt_marketdata" {
-  source              = "git::https://github.com/Energinet-DataHub/green-energy-hub-core.git//terraform/modules/service-bus-topic?ref=renetnielsen/add-service-bus-topic"
-  name                = "marketdata"
-  namespace_name      = module.sbn_inbound.name
-  resource_group_name = data.azurerm_resource_group.postoffice.name
-  dependencies        = [module.sbn_inbound]
-}
-
-module "sbnar_marketdata_listener" {
-  source                    = "git::https://github.com/Energinet-DataHub/green-energy-hub-core.git//terraform/modules/service-bus-topic-auth-rule?ref=renetnielsen/add-service-bus-topic"
-  name                      = "sbnar-inbound-listener"
-  namespace_name            = module.sbn_inbound.name
-  topic_name                = module.sbt_marketdata.name
-  resource_group_name       = data.azurerm_resource_group.postoffice.name
-  listen                    = true
-  dependencies              = [module.sbt_marketdata]
-}
-
-module "sbnar_marketdata_sender" {
-  source                    = "git::https://github.com/Energinet-DataHub/green-energy-hub-core.git//terraform/modules/service-bus-topic-auth-rule?ref=renetnielsen/add-service-bus-topic"
-  name                      = "sbnar-inbound-sender"
-  namespace_name            = module.sbn_inbound.name
-  topic_name                = module.sbt_marketdata.name
-  resource_group_name       = data.azurerm_resource_group.postoffice.name
-  send                      = true
-  dependencies              = [module.sbt_marketdata]
-}
