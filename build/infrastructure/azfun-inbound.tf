772d7ce35cb14b70ca31bdb461f9dd6ec34d201a
René T. Nielsen <renetdev@gmail.com>
2021-02-18 13:55:55 +0100

    fixes (#13)

--- build/infrastructure/azfun-inbound.tf
+++ build/infrastructure/azfun-inbound.tf
@@ -9,6 +9,12 @@ module "azfun_inbound" {
   application_insights_instrumentation_key  = module.appi_postoffice.instrumentation_key
   tags                                      = data.azurerm_resource_group.postoffice.tags
   app_settings                              = {
+    # Region: Default Values
+    WEBSITE_ENABLE_SYNC_UPDATE_SITE                   = true
+    WEBSITE_RUN_FROM_PACKAGE                          = 1
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE               = true
+    FUNCTIONS_WORKER_RUNTIME                          = "dotnet"
+    # Endregion
     MESSAGES_DB_CONNECTION_STRING                     = local.message_db_connection_string,
     MESSAGES_DB_NAME                                  = azurerm_cosmosdb_sql_database.db.name,
     INBOUND_QUEUE_MARKETDATA_TOPIC_NAME               = module.sbt_marketdata.name
