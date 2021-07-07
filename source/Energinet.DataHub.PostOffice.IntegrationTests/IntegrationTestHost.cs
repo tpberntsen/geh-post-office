2cd24a97924490b6ec58303aa87a6e6d2b5ec0d4
KarstenM <xnetsrak@gmail.com>
2021-07-07 10:06:21 +0200

    Changes to DataAvailable integration test after merge from main. Changes should be made when implementing correct integration test.

--- source/Energinet.DataHub.PostOffice.IntegrationTests/IntegrationTestHost.cs
+++ source/Energinet.DataHub.PostOffice.IntegrationTests/IntegrationTestHost.cs
@@ -15,13 +15,17 @@
 using System;
 using System.Net;
 using System.Threading;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
 using Energinet.DataHub.PostOffice.Application;
 using Energinet.DataHub.PostOffice.Application.DataAvailable;
+using Energinet.DataHub.PostOffice.Application.GetMessage.Interfaces;
 using Energinet.DataHub.PostOffice.Application.Validation;
 using Energinet.DataHub.PostOffice.Common;
 using Energinet.DataHub.PostOffice.Common.MediatR;
 using Energinet.DataHub.PostOffice.Inbound.Parsing;
 using Energinet.DataHub.PostOffice.Infrastructure;
+using Energinet.DataHub.PostOffice.Infrastructure.GetMessage;
 using Energinet.DataHub.PostOffice.Infrastructure.Mappers;
 using Energinet.DataHub.PostOffice.Infrastructure.Pipeline;
 using Energinet.DataHub.PostOffice.IntegrationTests.DataAvailable;
@@ -31,6 +35,7 @@ using Microsoft.Azure.Cosmos.Fluent;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using SimpleInjector;
+using SimpleInjector.Integration.ServiceCollection;
 using SimpleInjector.Lifestyles;
 using Xunit;
 using Xunit.Categories;
@@ -40,7 +45,7 @@ namespace Energinet.DataHub.PostOffice.IntegrationTests
 {
     [Collection("IntegrationTest")]
     [IntegrationTest]
-    public class IntegrationTestHost : IDisposable
+    public class IntegrationTestHost : IAsyncDisposable
     {
         private readonly Scope _scope;
         private readonly Container _container;
@@ -52,7 +57,7 @@ namespace Energinet.DataHub.PostOffice.IntegrationTests
             _container = new Container();
             var serviceCollection = new ServiceCollection();
 
-            serviceCollection.AddSimpleInjector(_container);
+            serviceCollection.AddSimpleInjector(_container, options => options.DisposeContainerWithServiceProvider = false);
             _serviceProvider = serviceCollection.BuildServiceProvider().UseSimpleInjector(_container);
 
             _container.Register<IValidator<DataAvailableCommand>, DataAvailableRuleSet>(Lifestyle.Scoped);
@@ -60,8 +65,13 @@ namespace Energinet.DataHub.PostOffice.IntegrationTests
             // Add Custom Services
             _container.Register<IMapper<Contracts.DataAvailable, DataAvailableCommand>, DataAvailableMapper>(Lifestyle.Scoped);
             _container.Register<IDocumentStore<Domain.DataAvailable>, CosmosDataAvailableStore>(Lifestyle.Scoped);
+            _container.Register<IDataAvailableStorageService, DataAvailableStorageService>(Lifestyle.Scoped);
             _container.Register<DataAvailableContractParser>(Lifestyle.Scoped);
+            _container.Register<ISendMessageToServiceBus, SendMessageToServiceBus>(Lifestyle.Scoped);
+            _container.Register<IGetPathToDataFromServiceBus, GetPathToDataFromServiceBus>(Lifestyle.Scoped);
+            _container.Register<IStorageService, StorageService>(Lifestyle.Scoped);
 
+            _container.Register<ServiceBusClient>(() => new ServiceBusClient("Endpoint=sb://test.windows.net/;SharedAccessKeyName=test;SharedAccessKey=test"), Lifestyle.Singleton);
             _container.Register<CosmosDatabaseConfig>(() => new CosmosDatabaseConfig("CHANGE_NAME"), Lifestyle.Singleton);
             serviceCollection.AddCosmosClientBuilder(useBulkExecution: false);
 
@@ -84,10 +94,11 @@ namespace Energinet.DataHub.PostOffice.IntegrationTests
             _scope = AsyncScopedLifestyle.BeginScope(_container);
         }
 
-        public void Dispose()
+        public async ValueTask DisposeAsync()
         {
             Dispose(true);
             GC.SuppressFinalize(this);
+            await _container.DisposeContainerAsync().ConfigureAwait(false);
         }
 
         protected virtual void Dispose(bool disposing)
@@ -128,7 +139,7 @@ namespace Energinet.DataHub.PostOffice.IntegrationTests
                 .ReturnsAsync(mockItemResponse.Object);
 
             return new CosmosClientWrapper(
-                "AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
+                "AccountEndpoint=https://localhost:8081/;AccountKey=1234",
                 mockContainer);
         }
     }
