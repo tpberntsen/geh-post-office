a5a2701b551dd2780e8d446e0fa0dbb31644e0ac
KarstenM <xnetsrak@gmail.com>
2021-07-16 14:34:30 +0200

    restructure for MessageReplyTableStorage

--- source/Energinet.DataHub.PostOffice.Infrastructure/MessageReplyStorage/MessageReplyTableStorage.cs
+++ source/Energinet.DataHub.PostOffice.Infrastructure/MessageReplyStorage/MessageReplyTableStorage.cs
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 using System;
+using System.Net;
 using System.Threading.Tasks;
 using Energinet.DataHub.PostOffice.Application.GetMessage.Interfaces;
 using Microsoft.WindowsAzure.Storage;
@@ -33,7 +34,7 @@ namespace Energinet.DataHub.PostOffice.Infrastructure.MessageReplyStorage
 
         public async Task<string?> GetMessageReplyAsync(string messageKey)
         {
-            var cloudTable = _serviceClient.GetTableReference("MessageReply");
+            var cloudTable = await InstantiateCloudTableAsync().ConfigureAwait(false);
 
             TableOperation getOperation = TableOperation.Retrieve<MessageReplyTableEntity>(messageKey, messageKey);
             TableResult operationResult = await cloudTable.ExecuteAsync(getOperation).ConfigureAwait(false);
@@ -46,15 +47,28 @@ namespace Energinet.DataHub.PostOffice.Infrastructure.MessageReplyStorage
         {
             if (contentUri is null) throw new ArgumentNullException(nameof(contentUri));
 
-            var cloudTable = _serviceClient.GetTableReference("MessageReply");
-
-            await cloudTable.CreateIfNotExistsAsync().ConfigureAwait(false);
+            var cloudTable = await InstantiateCloudTableAsync().ConfigureAwait(false);
 
             var messageReplyTableEntity = new MessageReplyTableEntity(messageKey, messageKey) { ContentPath = contentUri.AbsoluteUri };
-
             TableOperation tableOperation = TableOperation.InsertOrReplace(messageReplyTableEntity);
 
             var tableResult = await cloudTable.ExecuteAsync(tableOperation).ConfigureAwait(false);
+            ValidateTableOperationResult(tableResult);
+        }
+
+        private static void ValidateTableOperationResult(TableResult result)
+        {
+            if (result.Result is null)
+            {
+                throw new Exception($"Could not save MessageReply to storage, response code: {result.HttpStatusCode}");
+            }
+        }
+
+        private async Task<CloudTable> InstantiateCloudTableAsync()
+        {
+            var cloudTable = _serviceClient.GetTableReference("MessageReply");
+            await cloudTable.CreateIfNotExistsAsync().ConfigureAwait(false);
+            return cloudTable;
         }
     }
 }
