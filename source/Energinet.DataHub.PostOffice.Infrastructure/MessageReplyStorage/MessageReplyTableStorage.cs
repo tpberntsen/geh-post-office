6337a070b4eee0c21cff8c2d7587e53da33737b9
Jesper Justesen <juj@energinet.dk>
2021-08-09 15:40:45 +0200

    Initial replacement of packages and fixes of code lines

--- source/Energinet.DataHub.PostOffice.Infrastructure/MessageReplyStorage/MessageReplyTableStorage.cs
+++ source/Energinet.DataHub.PostOffice.Infrastructure/MessageReplyStorage/MessageReplyTableStorage.cs
@@ -15,8 +15,7 @@
 using System;
 using System.Threading.Tasks;
 using Energinet.DataHub.PostOffice.Application.GetMessage.Interfaces;
-using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Table;
+using Microsoft.Azure.Cosmos.Table;
 
 namespace Energinet.DataHub.PostOffice.Infrastructure.MessageReplyStorage
 {
