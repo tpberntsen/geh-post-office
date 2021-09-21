c615717cf1290cc879261699bb1f022b0bd8949c
Jesper Justesen <juj@energinet.dk>
2021-09-21 15:25:48 +0200

    Use correct interface for async dispose

--- source/PostOffice.Communicator/source/PostOffice.Communicator/DataAvailable/DataAvailableNotificationSender.cs
+++ source/PostOffice.Communicator/source/PostOffice.Communicator/DataAvailable/DataAvailableNotificationSender.cs
@@ -20,7 +20,7 @@ using GreenEnergyHub.PostOffice.Communicator.Model;
 
 namespace GreenEnergyHub.PostOffice.Communicator.DataAvailable
 {
-    public class DataAvailableNotificationSender : IDataAvailableNotificationSender, System.IDisposable
+    public class DataAvailableNotificationSender : IDataAvailableNotificationSender, System.IAsyncDisposable
     {
         private readonly ServiceBusClient _serviceBusClient;
         public DataAvailableNotificationSender(string connectionString)
