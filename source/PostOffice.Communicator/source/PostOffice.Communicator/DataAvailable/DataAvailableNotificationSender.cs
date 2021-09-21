9c19a3956bc5c2a66b5ac440a4a70c9e9e6aaac3
Michael Majgaard <52096428+michaelmajgaard@users.noreply.github.com>
2021-09-21 12:28:41 +0200

    M  PostOffice.Communicator/source/PostOffice.Communicator/DataAvailable/DataAvailableNotificationParser.cs M  PostOffice.Communicator/source/PostOffice.Communicator/DataAvailable/DataAvailableNotificationSender.cs

--- source/PostOffice.Communicator/source/PostOffice.Communicator/DataAvailable/DataAvailableNotificationSender.cs
+++ source/PostOffice.Communicator/source/PostOffice.Communicator/DataAvailable/DataAvailableNotificationSender.cs
@@ -1,4 +1,4 @@
-// // Copyright 2020 Energinet DataHub A/S
+﻿// // Copyright 2020 Energinet DataHub A/S
 // //
 // // Licensed under the Apache License, Version 2.0 (the "License2");
 // // you may not use this file except in compliance with the License.
@@ -20,7 +20,9 @@ using GreenEnergyHub.PostOffice.Communicator.Model;
 
 namespace GreenEnergyHub.PostOffice.Communicator.DataAvailable
 {
-    public class DataAvailableNotificationSender : IDataAvailableNotificationSender, System.IDisposable
+#pragma warning disable CA1001
+    public class DataAvailableNotificationSender : IDataAvailableNotificationSender
+#pragma warning restore CA1001
     {
         private readonly ServiceBusClient _serviceBusClient;
 
@@ -33,23 +35,23 @@ namespace GreenEnergyHub.PostOffice.Communicator.DataAvailable
         private string ServiceBusConnectionString { get; init; }
         public async Task SendAsync(DataAvailableNotificationDto dataAvailableNotificationDto)
         {
+            if (dataAvailableNotificationDto == null)
+                throw new ArgumentNullException(nameof(dataAvailableNotificationDto));
+
             await using var sender = _serviceBusClient.CreateSender("sbq-dataavailable");
             using var messageBatch = await sender.CreateMessageBatchAsync().ConfigureAwait(false);
             var msg = new Contracts.DataAvailableNotificationContract().ToByteArray();
             if (!messageBatch.TryAddMessage(new ServiceBusMessage(new BinaryData(msg))))
             {
+#pragma warning disable CA2201
                 throw new Exception("The message is too large to fit in the batch.");
+#pragma warning restore CA2201
             }
 
             Console.WriteLine(
                 $"Message added to batch, uuid: {dataAvailableNotificationDto.UUID}, recipient: {dataAvailableNotificationDto.Recipient} ");
 
             await sender.SendMessagesAsync(messageBatch).ConfigureAwait(false);
-        }
-
-        public void Dispose()
-        {
-            throw new System.NotImplementedException();
         }
     }
 }
