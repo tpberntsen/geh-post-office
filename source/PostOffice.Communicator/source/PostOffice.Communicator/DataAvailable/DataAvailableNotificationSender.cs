8da1f5c716f8b5fe4cf2d45d25410bed80206ad7
Jesper Justesen <juj@energinet.dk>
2021-09-21 11:37:47 +0200

    Basic implementation of DataavailableNotification

---
+++ source/PostOffice.Communicator/source/PostOffice.Communicator/DataAvailable/DataAvailableNotificationSender.cs
@@ -0,0 +1,55 @@
+// // Copyright 2020 Energinet DataHub A/S
+// //
+// // Licensed under the Apache License, Version 2.0 (the "License2");
+// // you may not use this file except in compliance with the License.
+// // You may obtain a copy of the License at
+// //
+// //     http://www.apache.org/licenses/LICENSE-2.0
+// //
+// // Unless required by applicable law or agreed to in writing, software
+// // distributed under the License is distributed on an "AS IS" BASIS,
+// // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// // See the License for the specific language governing permissions and
+// // limitations under the License.
+
+using System;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using Google.Protobuf;
+using GreenEnergyHub.PostOffice.Communicator.Model;
+
+namespace GreenEnergyHub.PostOffice.Communicator.DataAvailable
+{
+    public class DataAvailableNotificationSender : IDataAvailableNotificationSender, System.IDisposable
+    {
+        private readonly ServiceBusClient _serviceBusClient;
+
+        public DataAvailableNotificationSender(string connectionString)
+        {
+            ServiceBusConnectionString = connectionString;
+            _serviceBusClient = new ServiceBusClient(ServiceBusConnectionString);
+        }
+
+        private string ServiceBusConnectionString { get; init; }
+        public async Task SendAsync(DataAvailableNotificationDto dataAvailableNotificationDto)
+        {
+            await using var sender = _serviceBusClient.CreateSender("sbq-dataavailable");
+            using var messageBatch = await sender.CreateMessageBatchAsync().ConfigureAwait(false);
+            var msg = new Contracts.DataAvailableNotificationContract().ToByteArray();
+            if (!messageBatch.TryAddMessage(new ServiceBusMessage(new BinaryData(msg))))
+            {
+                throw new Exception("The message is too large to fit in the batch.");
+            }
+
+            Console.WriteLine(
+                $"Message added to batch, uuid: {dataAvailableNotificationDto.UUID}, recipient: {dataAvailableNotificationDto.Recipient} ");
+
+            await sender.SendMessagesAsync(messageBatch).ConfigureAwait(false);
+        }
+
+        public void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- source/PostOffice.Communicator/source/PostOffice.Communicator/Model/DataAvailableNotificationDto.cs
+++ source/PostOffice.Communicator/source/PostOffice.Communicator/Model/DataAvailableNotificationDto.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace GreenEnergyHub.PostOffice.Communicator.Model
 {
-    public sealed class DataAvailableNotificationDto
-    {
-    }
+    public sealed record DataAvailableNotificationDto(
+        string UUID, // Unique dataset identification
+        string Recipient, // Dataset recipient
+        string MessageType, // Dataset message type
+        string Origin, // Identification for where the dataset can be queried
+        bool SupportsBundling, // Is the message capable of being bundled with similar messages
+        int RelativeWeight);
 }
