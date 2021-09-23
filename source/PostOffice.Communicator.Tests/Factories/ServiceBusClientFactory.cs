2c94c447dc9c6788e90173a1738f987b819c2f8f
Michael Majgaard <52096428+michaelmajgaard@users.noreply.github.com>
2021-09-23 10:02:31 +0200

    A  source/PostOffice.Communicator.Tests/Factories/ServiceBusClientFactory.cs M  source/PostOffice.Communicator.Tests/PostOffice.Communicator.Tests.csproj

---
+++ source/PostOffice.Communicator.Tests/Factories/ServiceBusClientFactory.cs
@@ -0,0 +1,34 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Xunit;
+
+namespace GreenEnergyHub.PostOffice.Communicator.Factories
+{
+    public sealed class ServiceBusClientFactoryTests
+    {
+        [Fact]
+        public void Create_ReturnsServiceBusClient()
+        {
+            // arrange
+            var target = new ServiceBusClientFactory("Endpoint=sb://sbn-postoffice.servicebus.windows.net/;SharedAccessKeyName=Hello;SharedAccessKey=there");
+
+            // act
+            var actual = target.Create();
+
+            // assert
+            Assert.NotNull(actual);
+        }
+    }
+}
