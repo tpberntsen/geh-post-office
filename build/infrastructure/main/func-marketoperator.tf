4b5bf99a1773d6bcd740147e49cc86c216774c9f
René Nielsen <renetdev@gmail.com>
2021-11-09 01:04:40 +0100

    fix double function iac

--- build/infrastructure/main/func-marketoperator.tf
+++ build/infrastructure/main/func-marketoperator.tf
@@ -11,10 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-module "func_marketoperator_peek" {
+module "func_marketoperator" {
   source                                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/function-app?ref=5.1.0"
 
-  name                                      = "marketoperator-peek"
+  name                                      = "marketoperator"
   project_name                              = var.domain_name_short
   environment_short                         = var.environment_short
   environment_instance                      = var.environment_instance
