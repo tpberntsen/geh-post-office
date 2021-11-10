cd204db3f4644d1ee122b2b7dfc4b0841da73f81
Jesper Justesen <juj@energinet.dk>
2021-11-10 08:51:51 +0100

    Added using to CosmosClient instance

--- source/Energinet.DataHub.MessageHub.Client.Tests/Storage/BundleRepositoryTests.cs
+++ source/Energinet.DataHub.MessageHub.Client.Tests/Storage/BundleRepositoryTests.cs
@@ -38,7 +38,7 @@ namespace Energinet.DataHub.MessageHub.Client.Tests.Storage
                 string.Empty,
                 "AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
                 "post-office");
-            var cosmosClient = new CosmosClientBuilder("AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==")
+            using var cosmosClient = new CosmosClientBuilder("AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==")
                 .WithBulkExecution(false)
                 .WithSerializerOptions(new CosmosSerializationOptions { PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase })
                 .Build();
