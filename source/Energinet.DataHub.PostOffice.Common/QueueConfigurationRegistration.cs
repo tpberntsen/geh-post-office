b1d8b099c5da1adddc297d424c48ee208b44fb9f
Alexei Mihalchuk <AIU@energinet.dk>
2021-11-11 10:29:36 +0100

    Update default to new env.

--- source/Energinet.DataHub.PostOffice.Common/QueueConfigurationRegistration.cs
+++ source/Energinet.DataHub.PostOffice.Common/QueueConfigurationRegistration.cs
@@ -26,16 +26,16 @@ namespace Energinet.DataHub.PostOffice.Common
             container.RegisterSingleton(() =>
             {
                 var configuration = container.GetService<IConfiguration>();
-                var timeSeriesQueue = configuration.GetValue("TIMESERIES_QUEUE_NAME", "sbq-timeseries");
-                var timeSeriesReplyQueue = configuration.GetValue("TIMESERIES_REPLY_QUEUE_NAME", "sbq-timeseries-reply");
-                var chargesQueue = configuration.GetValue("CHARGES_QUEUE_NAME", "sbq-charges");
-                var chargesReplyQueue = configuration.GetValue("CHARGES_REPLY_QUEUE_NAME", "sbq-charges-reply");
-                var marketRolesQueue = configuration.GetValue("MARKETROLES_QUEUE_NAME", "sbq-marketroles");
-                var marketRolesReplyQueue = configuration.GetValue("MARKETROLES_REPLY_QUEUE_NAME", "sbq-marketroles-reply");
-                var meteringPointsQueue = configuration.GetValue("METERINGPOINTS_QUEUE_NAME", "sbq-meteringpoints");
-                var meteringPointsReplyQueue = configuration.GetValue("METERINGPOINTS_REPLY_QUEUE_NAME", "sbq-meteringpoints-reply");
-                var aggregationsQueue = configuration.GetValue("AGGREGATIONS_QUEUE_NAME", "sbq-aggregations");
-                var aggregationsReplyQueue = configuration.GetValue("AGGREGATIONS_REPLY_QUEUE_NAME", "sbq-aggregations-reply");
+                var timeSeriesQueue = configuration.GetValue("TIMESERIES_QUEUE_NAME", "timeseries");
+                var timeSeriesReplyQueue = configuration.GetValue("TIMESERIES_REPLY_QUEUE_NAME", "timeseries-reply");
+                var chargesQueue = configuration.GetValue("CHARGES_QUEUE_NAME", "charges");
+                var chargesReplyQueue = configuration.GetValue("CHARGES_REPLY_QUEUE_NAME", "charges-reply");
+                var marketRolesQueue = configuration.GetValue("MARKETROLES_QUEUE_NAME", "marketroles");
+                var marketRolesReplyQueue = configuration.GetValue("MARKETROLES_REPLY_QUEUE_NAME", "marketroles-reply");
+                var meteringPointsQueue = configuration.GetValue("METERINGPOINTS_QUEUE_NAME", "meteringpoints");
+                var meteringPointsReplyQueue = configuration.GetValue("METERINGPOINTS_REPLY_QUEUE_NAME", "meteringpoints-reply");
+                var aggregationsQueue = configuration.GetValue("AGGREGATIONS_QUEUE_NAME", "aggregations");
+                var aggregationsReplyQueue = configuration.GetValue("AGGREGATIONS_REPLY_QUEUE_NAME", "aggregations-reply");
 
                 return new PeekRequestConfig(
                     timeSeriesQueue,
@@ -53,11 +53,11 @@ namespace Energinet.DataHub.PostOffice.Common
             container.RegisterSingleton(() =>
             {
                 var configuration = container.GetService<IConfiguration>();
-                var timeSeriesDequeueQueue = configuration.GetValue("TIMESERIES_DEQUEUE_QUEUE_NAME", "sbq-timeseries-dequeue");
-                var chargesDequeueQueue = configuration.GetValue("CHARGES_DEQUEUE_QUEUE_NAME", "sbq-charges-dequeue");
-                var marketRolesDequeueQueue = configuration.GetValue("MARKETROLES_DEQUEUE_QUEUE_NAME", "sbq-marketroles-dequeue");
-                var meteringPointsDequeueQueue = configuration.GetValue("METERINGPOINTS_DEQUEUE_QUEUE_NAME", "sbq-meteringpoints-dequeue");
-                var aggregationsDequeueQueue = configuration.GetValue("AGGREGATIONS_DEQUEUE_QUEUE_NAME", "sbq-aggregations-dequeue");
+                var timeSeriesDequeueQueue = configuration.GetValue("TIMESERIES_DEQUEUE_QUEUE_NAME", "timeseries-dequeue");
+                var chargesDequeueQueue = configuration.GetValue("CHARGES_DEQUEUE_QUEUE_NAME", "charges-dequeue");
+                var marketRolesDequeueQueue = configuration.GetValue("MARKETROLES_DEQUEUE_QUEUE_NAME", "marketroles-dequeue");
+                var meteringPointsDequeueQueue = configuration.GetValue("METERINGPOINTS_DEQUEUE_QUEUE_NAME", "meteringpoints-dequeue");
+                var aggregationsDequeueQueue = configuration.GetValue("AGGREGATIONS_DEQUEUE_QUEUE_NAME", "aggregations-dequeue");
 
                 return new DequeueConfig(
                     timeSeriesDequeueQueue,
