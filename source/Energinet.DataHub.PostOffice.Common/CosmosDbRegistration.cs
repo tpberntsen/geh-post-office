ea1db4a7468aab6238da8d9a3d5542d8fa21fb53
KarstenM <xnetsrak@gmail.com>
2021-11-16 10:46:10 +0100

    removed Cosmos setup, WithContentResponseOnWrite. Because it didn't return exceptions.

--- source/Energinet.DataHub.PostOffice.Common/CosmosDbRegistration.cs
+++ source/Energinet.DataHub.PostOffice.Common/CosmosDbRegistration.cs
@@ -58,7 +58,6 @@ namespace Energinet.DataHub.PostOffice.Common
             var cosmosClient = new CosmosClientBuilder(connectionString)
                 .WithBulkExecution(bulkConfiguration)
                 .WithSerializerOptions(new CosmosSerializationOptions { PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase })
-                .WithContentResponseOnWrite(false)
                 .Build();
 
             return new CosmosClientProvider(cosmosClient);
