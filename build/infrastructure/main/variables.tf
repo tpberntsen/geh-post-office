db9a628c6bc0d9d775c926fe54174c1ba9bf237c
Kristian Schneider <khs@thamesconsulting.com>
2022-01-27 10:40:24 +0100

    .

--- build/infrastructure/main/variables.tf
+++ build/infrastructure/main/variables.tf
@@ -16,6 +16,10 @@ variable subscription_id {
   description = "Subscription that the infrastructure code is deployed into."
 }
 
+variable project_name{
+    type        = string
+    description = "Name of the project that the infrastructure code is deployed into."
+}
 variable resource_group_name {
   type        = string
   description = "Resource Group that the infrastructure code is deployed into."
