26e7cc2735194432f819cc7ae83c7ce6c25c10f6
Dan Stenrøjl <XDAST@energinet.dk>
2022-02-03 09:22:16 +0100

    What: Avoid count

--- build/infrastructure/main/cosmos-postoffice.tf
+++ build/infrastructure/main/cosmos-postoffice.tf
@@ -148,15 +148,20 @@ resource "azurerm_private_endpoint" "cosmos_sql" {
 
 # Create A records pointing to the Cosmos SQL private endpoint.
 # Multiple DNS records will be created as Cosmos must have a global FQDN and a local per region.
-resource "azurerm_private_dns_a_record" "cosmosdb_sql" {
-  count               = length(azurerm_private_endpoint.cosmos_sql.custom_dns_configs)
-
-  # azurerm_cosmosdb_account.post_office.name
-  name                = azurerm_private_endpoint.cosmos_sql.custom_dns_configs[count.index].fqdn
+resource "azurerm_private_dns_a_record" "cosmosdb_sql_global" {
+  name                = azurerm_private_endpoint.cosmos_sql.custom_dns_configs[0].fqdn
   zone_name           = "privatelink.documents.azure.com"
   resource_group_name = data.azurerm_key_vault_secret.pdns_resource_group_name.value
   ttl                 = 3600
 
-  # [azurerm_private_endpoint.cosmos_sql.private_service_connection[0].private_ip_address]
-  records             = azurerm_private_endpoint.cosmos_sql.custom_dns_configs[count.index].ip_addresses[0]
+  records             = azurerm_private_endpoint.cosmos_sql.custom_dns_configs[0].ip_addresses[0]
+}
+
+resource "azurerm_private_dns_a_record" "cosmosdb_sql_region" {
+  name                = azurerm_private_endpoint.cosmos_sql.custom_dns_configs[1].fqdn
+  zone_name           = "privatelink.documents.azure.com"
+  resource_group_name = data.azurerm_key_vault_secret.pdns_resource_group_name.value
+  ttl                 = 3600
+
+  records             = azurerm_private_endpoint.cosmos_sql.custom_dns_configs[1].ip_addresses[0]
 }
