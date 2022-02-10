4afe4e89d86c9e755ab90711fb454e1a0a8cc377
Dan Stenrøjl <XDAST@energinet.dk>
2022-02-10 12:17:28 +0100

    Fix local name

--- build/infrastructure/main/func-operations.tf
+++ build/infrastructure/main/func-operations.tf
@@ -47,7 +47,7 @@ module "func_operations" {
     RequestResponseLogContainerName           = data.azurerm_key_vault_secret.st_market_operator_logs_container_name.value
     B2C_TENANT_ID                             = data.azurerm_key_vault_secret.b2c_tenant_id.value
     BACKEND_SERVICE_APP_ID                    = data.azurerm_key_vault_secret.backend_service_app_id.value
-    SQL_ACTOR_DB_CONNECTION_STRING            = local.sql_actor_db_connection_string
+    SQL_ACTOR_DB_CONNECTION_STRING            = local.SQL_ACTOR_DB_CONNECTION_STRING
   }
 
   tags                                      = azurerm_resource_group.this.tags
